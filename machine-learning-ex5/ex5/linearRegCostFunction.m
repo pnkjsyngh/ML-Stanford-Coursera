f980febe2fd51ba55d4af821e3a044a1b6e31a4a
pnkjsyngh <pnkj.syngh@gmail.com>
2020-10-31 08:14:57 -0400

    Finished assignement 8

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -20,15 +20,28 @@ grad = zeros(size(theta));
 %
 
 h = X*theta;
-var = h-y;
-J = (var'*var)/2/m + (lambda/2/m)*theta(2:end)'*theta(2:end);
+var1 = h-y;
+J = (var1'*var1)/2/m + (lambda/2/m)*theta(2:end)'*theta(2:end);
 
-var = (X'*(h - y))/m;
-grad(1) = var(1);
+var2 = (X'*(h - y))/m;
+grad(1) = var2(1);
 
-var = ((X'*(h - y)) + lambda*theta)/m ;
-grad(2:end) = var(2:end);
+var3 = ((X'*(h - y)) + lambda*theta)/m ;
+grad(2:end) = var3(2:end);
 
+fprintf("\n\n")
+fprintf("Dimensions of X = %i x %i \n\n", size(X,1), size(X,2))
+
+fprintf("Dimensions of (h - y) = %i x %i \n\n\n\n", size(h-y,1), size(h-y,2))
+
+fprintf("Dimensions of X'.(h - y) = %i x %i \n\n", size(X'*(h - y),1), size(X'*(h - y),2))
+fprintf("Value of X'.(h - y) = [%f ; %f] \n\n", X'*(h - y))
+
+fprintf("\n\n")
+fprintf("Dimensions of (h -y).*X = %i x %i \n\n", size((h -y).*X,1), size((h -y).*X,2))
+fprintf("Dimensions of sum((h -y).*X) = %i x %i \n\n", size(sum((h - y).*X),1), size(sum((h - y).*X),2))
+var = sum((h - y).*X);
+fprintf("Value of sum((h -y).*X)) = [%f , %f] \n\n", var)
 
 
 
